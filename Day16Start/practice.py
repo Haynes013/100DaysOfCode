6695e4bd30bd3e485b515fe2105c7995294b9d3d
Mark A. Haynes Jr. <mhaynes031395@gmail.com>
2026-02-19 17:42:57 -0500

    OOP coffe machine, finished day 12

--- Day16Start/practice.py
+++ Day16Start/practice.py
@@ -13,4 +13,10 @@ my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
 
-import prettytable
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.border = True
+table.align = "l"
+print(table)
